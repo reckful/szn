d16314428beaee5263c0904e2d691ef0d2fb80c0
Atticus <atticusstormpirrie@gmail.com>
2018-02-09 16:35:05 +0000

    added to github

---
+++ news/.~c9_invoke_wObPfc.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel
+from wagtail.wagtailsearch import index
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+
+
+class NewsIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super(NewsIndexPage, self).get_context(request)
+        newspages = self.get_children().live().order_by('-first_published_at')
+        context['newspages'] = newspages
+        return context
+
+
+# ... (Keep the definition of NewsIndexPage)
+
+
+class ArticlePageTag(TaggedItemBase):
+    content_object = ParentalKey('ArticlePage', related_name='tagged_items')
+
+
+# Keep the definition of NewsIndexPage, and add:
+
+
+class ArticlePage(Page):
+    date = models.DateField("Post date")
+    body = RichTextField(blank=True)
+    coin_one = models.CharField(max_length=250, blank=True)
+    coin_two = models.CharField(max_length=250, blank=True)
+    coin_three = models.CharField(max_length=250, blank=True)
+    coin_four = models.CharField(max_length=250, blank=True)
+    tags = ClusterTaggableManager(through=ArticlePageTag, blank=True)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text='Landscape mode only; horizontal width between 1000px and 3000px.'
+    )
+
+    search_fields = Page.search_fields + [
+        index.SearchField('body'),
+    ]
+
+        FieldPanel('coin_four')
+        MultiFieldPanel([
+            FieldPanel('date'),
+            FieldPanel('tags'),
+        ], heading="Article information"),
+        ImageChooserPanel('image'),
+        FieldPanel('body'),
+        MultiFieldPanel([
+            FieldPanel('coin_one'),
+            FieldPanel('coin_two'),
+            FieldPanel('coin_three'),
+            FieldPanel('coin_four'),
+        ], heading="Coins mentioned"),
+    ]
+    
+    def get_tags(self):
+        """
+        Similar to the authors function above we're returning all the tags that
+        are related to the blog post into a list we can access on the template.
+        We're additionally adding a URL to access BlogPage objects with that tag
+        """
+        tags = self.tags.all()
+        for tag in tags:
+            tag.url = '/'+'/'.join(s.strip('/') for s in [
+                self.get_parent().url,
+                'tags',
+                tag.slug
+            ])
+        return tags
+
+    # Specifies parent to BlogPage as being BlogIndexPages
+    parent_page_types = ['BlogIndexPage']
+
+    # Specifies what content types can exist as children of BlogPage.
+    # Empty list means that no child content types are allowed.
+    subpage_types = []
---
+++ news/models.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import Tag, TaggedItemBase
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel
+from wagtail.wagtailsearch import index
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+
+from wagtail.contrib.wagtailroutablepage.models import RoutablePageMixin, route
+from wagtail.wagtailcore.models import Page
+from django.shortcuts import redirect, render
+from django.contrib import messages
+
+
+class NewsIndexPage(RoutablePageMixin, Page):
+    intro = RichTextField(blank=True)
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super(NewsIndexPage, self).get_context(request)
+        newspages = self.get_children().live().order_by('-first_published_at')
+        context['newspages'] = newspages
+        return context
+        
+    @route('^tags/$', name='tag_archive')
+    @route('^tags/(\w+)/$', name='tag_archive')
+    def tag_archive(self, request, tag=None):
+
+        try:
+            tag = Tag.objects.get(slug=tag)
+        except Tag.DoesNotExist:
+            if tag:
+                msg = 'There are no articles tagged with "{}"'.format(tag)
+                messages.add_message(request, messages.INFO, msg)
+            return redirect(self.url)
+
+        posts = self.get_posts(tag=tag)
+        context = {
+            'tag': tag,
+            'posts': posts
+        }
+        return render(request, 'news/news_index_page.html', context)
+
+    def serve_preview(self, request, mode_name):
+        # Needed for previews to work
+        return self.serve(request)
+        
+    def children(self):
+        return self.get_children().specific().live()
+        
+    def get_posts(self, tag=None):
+        posts = ArticlePage.objects.live().descendant_of(self)
+        if tag:
+            posts = posts.filter(tags=tag)
+        return posts
+
+    # Returns the list of Tags for all child posts of this BlogPage.
+    def get_child_tags(self):
+        tags = []
+        for post in self.get_posts():
+            # Not tags.append() because we don't want a list of lists
+            tags += post.get_tags
+        tags = sorted(set(tags))
+        return tags
+
+
+# ... (Keep the definition of NewsIndexPage)
+
+
+class ArticlePageTag(TaggedItemBase):
+    content_object = ParentalKey('ArticlePage', related_name='tagged_items')
+
+
+# Keep the definition of NewsIndexPage, and add:
+
+
+class ArticlePage(Page):
+    date = models.DateField("Post date")
+    body = RichTextField(blank=True)
+    coin_one = models.CharField(max_length=250, blank=True)
+    coin_two = models.CharField(max_length=250, blank=True)
+    coin_three = models.CharField(max_length=250, blank=True)
+    coin_four = models.CharField(max_length=250, blank=True)
+    tags = ClusterTaggableManager(through=ArticlePageTag, blank=True)
+    featured = models.BooleanField(default=False, blank=True)
+    author = RichTextField(blank=True)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text='Landscape mode only; horizontal width between 1000px and 3000px.'
+    )
+
+    search_fields = Page.search_fields + [
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            FieldPanel('date'),
+            FieldPanel('tags'),
+            FieldPanel('featured'),
+            FieldPanel('author'),
+        ], heading="Article information"),
+        ImageChooserPanel('image'),
+        FieldPanel('body'),
+        MultiFieldPanel([
+            FieldPanel('coin_one'),
+            FieldPanel('coin_two'),
+            FieldPanel('coin_three'),
+            FieldPanel('coin_four'),
+        ], heading="Coins mentioned"),
+    ]
+    
+    @property
+    def get_tags(self):
+        """
+        Similar to the authors function above we're returning all the tags that
+        are related to the blog post into a list we can access on the template.
+        We're additionally adding a URL to access BlogPage objects with that tag
+        """
+        tags = self.tags.all()
+        for tag in tags:
+            tag.url = '/'+'/'.join(s.strip('/') for s in [
+                self.get_parent().url,
+                'tags',
+                tag.slug
+            ])
+        return tags
+
+    # Specifies parent to BlogPage as being BlogIndexPages
+    parent_page_types = ['NewsIndexPage']
+
+    # Specifies what content types can exist as children of BlogPage.
+    # Empty list means that no child content types are allowed.
+    subpage_types = []
+    
+    def get_posts(self, tag=None):
+        posts = ArticlePage.objects.live().descendant_of(self.get_parent().url)
+        if tag:
+            posts = posts.filter(tags=tag)
+        return posts
+        
+    def get_child_tags(self):
+        tags = []
+        for post in self.get_posts():
+            # Not tags.append() because we don't want a list of lists
+            tags += post.get_tags
+        tags = sorted(set(tags))
+        return tags
