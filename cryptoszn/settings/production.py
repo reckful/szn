6f1127c2ed10342b54bfa01562f700071cc87cbf
Atticus <atticusstormpirrie@gmail.com>
2018-02-10 19:16:58 +0000

    removed gitignore and changed production.py

--- cryptoszn/settings/production.py
+++ cryptoszn/settings/production.py
@@ -2,7 +2,18 @@ from __future__ import absolute_import, unicode_literals
 
 from .base import *
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '5q35)sa8^)a+*7*i-=#nzdnzyg6-g3^uhm_c^!1q!60^a*pr0n'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ALLOWED_HOSTS = ['www.cryptoszn.com']
+SECURE_SSL_REDIRECT = True
+
 
 try:
     from .local import *
