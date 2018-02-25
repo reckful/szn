d1c2e4c635874ac90a74c162a2ce0357bc5ca68f
Atticus <atticusstormpirrie@gmail.com>
2018-02-25 13:56:39 +0000

    fixed image sizing on icos, exchanges, and wallets.

--- cryptoszn/settings/production.py
+++ cryptoszn/settings/production.py
@@ -8,7 +8,6 @@ DEBUG = False
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '5q35)sa8^)a+*7*i-=#nzdnzyg6-g3^uhm_c^!1q!60^a*pr0n'
 
-
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ALLOWED_HOSTS = ['www.cryptoszn.com']
